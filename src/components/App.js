3253b4ee087871a594fa2ad71e50d937b260b1c7
deept22R <deepti220398@gmail.com>
2021-02-10 02:58:15 +0530

    modified code

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,113 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
+const App=()=>{
+    const [renderBall,setRenderBall] = useState(false);
+    const [posi,setPosi] = useState(0);
+    const [ballPosition,setBallPosition] = useState({left:0,top:0});
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
-    }
+const buttonClickHandler=()=>{
+    setRenderBall(true);
+};
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+function rightKeyPress(event){
+    if(event.keyCode==39){
+        setBallPosition({
+            left:ballPosition.left+5,
+        });
     }
 }
 
+useEffect(()=>{
+    document.addEventListener("keydown", rightKeyPress);
+    // cleanup function
+    //and this will store in 1st use of useEffect and then when again useEffect is used then it will executed first
+    return()=>{
+        document.removeEventListener('keydown',rightKeyPress);
+    }    
+},[ballPosition]);
+
+const renderBallOrButton=()=> {
+     if (renderBall) {
+         return <div className="ball" 
+         style={{
+             left: ballPosition.left + "px",
+             top: ballPosition.top + "px",
+             position: "absolute",
+         }}></div>;
+        //  style is object
+     } else {
+         return <button onClick={buttonClickHandler}>Click For One Ball</button>
+     }
+ };
+ 
+    return <div className="playground">{renderBallOrButton()}</div>;
+};
 
 export default App;
+
+
+ // if(event.keyCode==39){
+        //         setBallPosition({
+        //             left: ballPosition.left + 5,
+        //             top: ballPosition.top, 
+        //         });
+            // case 40:
+            //     setBallPosition({
+            //         left: ballPosition.left,
+            //         top: ballPosition.top+5,
+            //     });
+            //     break;
+            // case 37:
+            //     setBallPosition({
+            //         left: ballPosition.left-5,
+            //         top: ballPosition.top,
+            //     });
+            //     break;
+            // case 38:
+            //     setBallPosition({
+            //         left: ballPosition.left,
+            //         top: ballPosition.top-5,
+            //     })
+//if arrow key is pressed event should happen so attack keydowm event listner
+
+
+
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             renderBall: false,
+//             posi : 0,
+//             ballPosition: { left: "0px" }
+//         };
+//         this.renderChoice = this.renderBallOrButton.bind(this)
+//         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+//     };
+
+//     buttonClickHandler() {
+   
+//    }
+//     renderBallOrButton() {
+// 		if (this.state.renderBall) {
+// 		    return <div className="ball" style={this.state.ballPosition}></div>
+// 		} else {
+// 		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+// 		}
+//     }
+
+    // bind ArrowRight keydown event
+//     componentDidMount() {
+      
+//     }
+
+//     render() {
+//         return (
+//             <div className="playground">
+//                 {this.renderBallOrButton()}
+//             </div>
+//         )
+//     }
+// }
+
